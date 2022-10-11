c5127f355ef1cee0949edc712eee72380ae525b8
Liam Greenlee <liam.a.greenlee@nasa.gov>
2022-10-11 10:16:54 -0400

    Small usability fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,11 +14,11 @@ done
 if [ $fflag = true ]; then
 	echo "creating environment"
     /opt/conda/bin/conda env create --name mounted --file=/home/nvim-user/.config/container-files/environment.yml
-    /opt/conda/bin/conda run --no-capture-output -n mounted nvim
+    /opt/conda/bin/conda run --no-capture-output -n mounted nvim mount
 elif [ ! $nflag = true ]; then
 	echo "using mounted environment"
-    /opt/conda/bin/conda run --no-capture-output -n mounted nvim
+    /opt/conda/bin/conda run --no-capture-output -n mounted nvim mount
 else
 	echo "no environment"
-	nvim
+	nvim mount
 fi
--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -2,7 +2,7 @@ local utils = {}
 local home = os.getenv('HOME')
 
 function utils.get_config_path()
-    return home .. '/.config/nvim'
+    return vim.fn.stdpath('config') .. '/.config/nvim'
 end
 
 return utils
